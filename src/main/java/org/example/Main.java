650d75956bfbc725b692756866fd6f22c0464b48
S_LOKESH <149285495+S-Lokesh96@users.noreply.github.com>
2024-10-27 17:27:27 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,7 @@ package org.example;
 import java.sql.Connection;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         Connection con = Connections.getCon();
@@ -98,6 +97,6 @@ public class Main {
 //            default:
 //                System.out.println("Enter valid choice");
         }
-//        DAO.selectQuery(con);
+
     }
 }
